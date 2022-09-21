d71d23a6154a0634c1b7f68c97286e4e426ba2db
Thawane <thawaneapoliveira@gmail.com>
2022-09-21 13:27:38 -0300

    Adiciona requisito 10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
-    const mission = 'Missões';
+    const missionProp = 'Missões';
     return (
       <div data-testid="missions">
-        <Title headline={ mission } />
+        <Title headline={ missionProp } />
+        {missions.map((mission) => (
+          <MissionCard
+            key={ mission.name }
+            name={ mission.name }
+            year={ mission.year }
+            country={ mission.country }
+            destination={ mission.destination }
+
+          />
+        ))}
+        ;
       </div>
     );
   }
