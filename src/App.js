21d5c3a18df4a5930327f3f7e818878bcedb4647
Thawane <thawaneapoliveira@gmail.com>
2022-09-21 15:01:29 -0300

    Adiciona correção de requisitos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Header from './components/Header';
+import Missions from './components/Missions';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <div>
+        <Header />
+        <SolarSystem />
+        <Missions />
+      </div>
+    );
   }
 }
 
